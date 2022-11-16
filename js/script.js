b9614ffe84e6001e425a695a0b1989ffde038f51
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-15 22:15:38 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-document.querySelector("")
+const spreadMenu = document.querySelector("#spreadMenu");
+
+function openMenu () {
+    let menu = document.querySelector(".sidebar");
+    menu.style.right = "0px";
+}
+
+spreadMenu.addEventListener("click", openMenu);
+
+const closeButton = document.querySelector("#closeButton");
+
+function closeMenu () {
+    let menu = document.querySelector(".sidebar");
+    menu.style.right = "-300px";
+}
+
+closeButton.addEventListener("click", closeMenu);
