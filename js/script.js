1dc8b80c73fecbc8ed282de65506dbdd3c3fc536
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-16 12:40:50 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const spreadMenu = document.querySelector("#spreadMenu");
 
-function openMenu () {
+function openMenu() {
     let menu = document.querySelector(".sidebar");
     menu.style.right = "0px";
 }
@@ -9,9 +9,35 @@ spreadMenu.addEventListener("click", openMenu);
 
 const closeButton = document.querySelector("#closeButton");
 
-function closeMenu () {
+function closeMenu() {
     let menu = document.querySelector(".sidebar");
     menu.style.right = "-300px";
 }
 
 closeButton.addEventListener("click", closeMenu);
+
+// Destination 
+
+// const mars = document.querySelector("#planetMars");
+
+// function openMars() {
+//     let marte = document.querySelector(".mars-info");
+//     marte.style.display = "flex";
+//     let lua = document.querySelector(".moon-info");
+//     lua.style.display = "none";
+//     let euro = document.querySelector (".europa-info")
+//     euro.style.display = "none";
+// }
+
+// mars.addEventListener("click", openMars);
+
+// //
+
+// const europa = document.querySelector("#planetEuropa");
+
+// function openEuropa() {
+//     let euro = document.querySelector(".europa-info");
+//     euro.style.display = "flex";
+// }
+
+// europa.addEventListener("click", openEuropa);
