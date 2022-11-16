8f4374ec3617c7ea65d00f8c20675199b029273c
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-16 16:10:56 -0300

    remoçao de codigos

--- js/script.js
+++ js/script.js
@@ -15,29 +15,3 @@ function closeMenu() {
 }
 
 closeButton.addEventListener("click", closeMenu);
-
-// Destination 
-
-// const mars = document.querySelector("#planetMars");
-
-// function openMars() {
-//     let marte = document.querySelector(".mars-info");
-//     marte.style.display = "flex";
-//     let lua = document.querySelector(".moon-info");
-//     lua.style.display = "none";
-//     let euro = document.querySelector (".europa-info")
-//     euro.style.display = "none";
-// }
-
-// mars.addEventListener("click", openMars);
-
-// //
-
-// const europa = document.querySelector("#planetEuropa");
-
-// function openEuropa() {
-//     let euro = document.querySelector(".europa-info");
-//     euro.style.display = "flex";
-// }
-
-// europa.addEventListener("click", openEuropa);
